2d44196b4a6c46a791e3a2f2c4163cddf455fa78
ExtremBot1234 <44088452+ExtremBot1234@users.noreply.github.com>
2018-11-09 02:46:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,87 @@
-     client.on('message', message => {
-                  if(!message.channel.guild) return;
-        var prefix = "!";
-        if(message.content.startsWith(prefix + 'obc')) {
-        if(!message.channel.guild) return message.channel.send('**This is only for servers**').then(m => m.delete(5000));
-      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**Unfortunately you do not have permission** `ADMINISTRATOR`' );
-        let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-        let copy = "1 - Dream";
-        let request = `Requested By ${message.author.username}`;
-        if (!args) return message.reply('**You must type a word or phrase to send the podcast**');message.channel.send(`**Are you sure you are sending your podcast?؟ \nPodcast content:** \` ${args}\``).then(msg => {
-        msg.react('?')
-        .then(() => msg.react('?'))
-        .then(() =>msg.react('?'))
-         
-        let reaction1Filter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
-        let reaction2Filter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
-         
-        let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-        let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-         
-        reaction1.on("collect", r => {
-        message.channel.send(`? | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
-        message.guild.members.forEach(m => {
-        var bc = new
-           Discord.RichEmbed()
-           .setColor('RANDOM')
-           .setTitle('Broadcast')
-           .addField('Server', message.guild.name)
-           .addField('Sender', message.author.username)
-           .addField('Message', args)
-           .setThumbnail(message.author.avatarURL)
-           .setFooter(copy, client.user.avatarURL);
-        m.send({ embed: bc })
-        msg.delete();
-        })
-        })
-        reaction2.on("collect", r => {
-        message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-        msg.delete();
-        })
-        })
-        }
-        });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "&";
+client.on('message', message => { // Leaked by [ @M3a4x ]
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**This is only for servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return
+const args = message.content.split(" ").slice(1).join(" ")
+const BcList = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.setAuthor(`Message content : ${args}`)
+.setDescription(`**Broadcast with Ambed 📝\nBrodcast without Empad✏ \nYou have a minute to choose before you cancel the podcast**`)
+if (!args) return message.reply('**You must type a word or phrase to send the podcast**');message.channel.send(BcList).then(msg => {
+msg.react('📝')
+.then(() => msg.react('✏'))
+.then(() =>msg.react('📝'))
+ 
+var EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+var NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+ 
+var EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+var NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+ 
+ 
+EmbedBc.on("collect", r => {
+ 
+message.channel.send(`:ballot_box_with_check: Message sent successfully`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription(EmbedRep)
+.setThumbnail(message.author.avatarURL)
+m.send({ embed: bc })
+msg.delete();
+})
+})
+NormalBc.on("collect", r => {
+  message.channel.send(`:ballot_box_with_check: Message sent successfully`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+m.send(NormalRep);
+msg.delete();
+})
+})
+})
+}
+});
+
+const devs = ['492813868955992074'];
+const adminprefix = "a";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} ply .. ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} wt .. ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} ls .. ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+      message.channel.sendMessage(`**  ${argresult} st .. ✅**`)
+    }
+  if (message.content.startsWith(adminprefix + 'us')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**us ..**${argresult}** `)
+  } else
+  if (message.content.startsWith(adminprefix + 'av')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**avatar ... :** `);
+  }
+  });
+
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
